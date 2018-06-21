ab9605b852b810e4a0839ed30cd18e686abebedf
Tim Hopper <tdhopper@gmail.com>
2018-06-21 14:28:02 -0700

    swap theme

--- conf.py
+++ conf.py
@@ -140,7 +140,7 @@ NAVIGATION_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "bootstrap4"
+THEME = "bootblog4"
 
 # Primary color of your theme. This will be used to customize your theme and
 # auto-generate related colors in POSTS_SECTION_COLORS. Must be a HEX value.
